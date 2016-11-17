4f62d129871336932e70c233e31524eb75264994
Janek K <janek.kossinski@gmail.com>
2016-11-18 01:36:08 +0200

    Project start
    Started page build. Routing added

---
+++ js/viewRouter.js
@@ -0,0 +1,19 @@
+angular.module('jovfinance', ["ngRoute"])
+    .config(function($routeProvider) {
+        $routeProvider
+        .when("/", {
+            templateUrl : "./views/main.html"
+        })
+        .when("/us", {
+            templateUrl : "./views/us.html"
+        })
+        .when("/services", {
+            templateUrl : "./views/services.html"
+        })
+        .when("/price", {
+            templateUrl : "./views/price.html"
+        })
+        .when("/contact", {
+            templateUrl : "./views/contact.html"
+        });
+    });
