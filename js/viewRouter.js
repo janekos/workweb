bd1a3738c7d6773e401591224abb154a7a6abcd8
Janek K <janek.kossinski@gmail.com>
2016-12-02 00:40:23 +0200

    nav
    nav skeleton more or less complete

--- js/viewRouter.js
+++ js/viewRouter.js
@@ -1,4 +1,4 @@
-angular.module('jovfinance', ["ngRoute"])
+angular.module('jovfinance', ['ngRoute', 'ngMaterial'])
     .config(function($routeProvider) {
         $routeProvider
         .when("/", {
@@ -15,5 +15,42 @@ angular.module('jovfinance', ["ngRoute"])
         })
         .when("/contact", {
             templateUrl : "./views/contact.html"
+        });
+    })
+    .controller('mainPage', function($scope){
+    
+        $scope.activeLang = "ee";   
+        $scope.inactiveLang1 = "ru";   
+        $scope.inactiveLang2 = "gb";   
+    
+        $scope.responsive = function(){        
+            var x = $('.navContent');
+            if (x.hasClass("responsive")) {
+                x.removeClass("responsive");
+            } else {
+                x.addClass("responsive");
+            }
+        };
+        
+        $('.navWrapper').click(function(e){
+            $('.nav-item').each(function(){
+                if($(this).hasClass('selected')){
+                    $(this).removeClass('selected');
+                }
+            });
+            
+            if(e.target.id != "logo" && e.target.class != "langshid" && e.target.id != "iconin"){
+                try{
+                    $('#'+e.target.id).addClass("selected");
+                }catch(err){}
+            }
+        });
+    
+        $("#lang").click(function(){
+            if($(".langshid").css("display") == "none"){
+                $(".langshid").show(100);
+            }else{
+                $(".langshid").hide(100);
+            }
         });
     });
